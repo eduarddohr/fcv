2602bbc0ad68229aaf714925b6157190370cbe08
eduarddohr <eduarddohr@gmail.com>
2020-10-28 15:56:22 +0200

    implement chain transformations for images, implement new geometric transformations

--- FCV1/main.py
+++ FCV1/main.py
@@ -5,23 +5,69 @@ import os
 import shutil
 import numpy as np
 
+
+def BoxFilter(imageToTransform, parameter):
+    kernel = np.ones((int(parameter), int(parameter)))
+    kernel = kernel / np.sum(kernel)
+    return cv2.filter2D(imageToTransform, -1, kernel)
+
+
+def HighPass(imageToTransform, parameter):
+    kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    return cv2.filter2D(imageToTransform, -1, kernel)
+
+
+def Sharpening(imageToTransform, parameter):
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    return cv2.filter2D(imageToTransform, -1, kernel)
+
+
+def Rotate(imageToTransform, angle):
+    image_center = tuple(np.array(imageToTransform.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, int(angle), 1.0)
+    result = cv2.warpAffine(imageToTransform, rot_mat, imageToTransform.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
+
+
+def Translate(image, parameters):
+    height, width = image.shape[:2]
+    translation = np.float32([[1, 0, parameters[0]], [0, 1, parameters[1]]])
+    return cv2.warpAffine(image, translation, (width, height))
+
+
+def Shear(image, parameters):
+    height, width = image.shape[:2]
+    translation = np.float32([[1, parameters[0], 0], [parameters[1], 1, 0]])
+    return cv2.warpAffine(image, translation, (width, height))
+
+
+def Flip(image, parameters):
+    return cv2.flip(image, int(parameters[0]))
+
+
 def applyTransformations(imageToTransform, transformation, parameter):
     if transformation == "BoxFilter":
-        kernel = np.ones((int(parameter), int(parameter)))
-        kernel = kernel/ np.sum(kernel)
-        return cv2.filter2D(imageToTransform, -1, kernel)
+        return BoxFilter(imageToTransform, parameter[0])
     elif transformation == "HighPass":
-        kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
-        return cv2.filter2D(imageToTransform, -1, kernel)
-    elif transformation == "Sharpenning":
-        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-        return cv2.filter2D(imageToTransform, -1, kernel)
+        return HighPass(imageToTransform, parameter[0])
+    elif transformation == "Sharpening":
+        return Sharpening(imageToTransform, parameter[0])
+    elif transformation == "Rotate":
+        return Rotate(imageToTransform, parameter[0])
+    elif transformation == "Translate":
+        return Translate(image, parameter)
+    elif transformation == "Shear":
+        return Shear(image, parameter)
+    elif transformation == "Flip":
+        return Flip(image, parameter)
+
 
 directory = filedialog.askdirectory()
 # print(directory)
 if directory:
     imagesList = glob.glob(directory + "/*.jpg")
     newDir = directory + "_aug"
+    appliedTransformations = ""
     if os.path.isdir(newDir):
         shutil.rmtree(newDir)
     os.mkdir(newDir)
@@ -29,14 +75,19 @@ if directory:
     configFile = open(directory + "/config.txt")
     configFileLines = configFile.readlines()
     for line in configFileLines:
-        option = line.split(':')
         counter = 1
         for imagePath in imagesList:
             image = cv2.imread(imagePath)
             imageName = imagePath.split("\\")
             imageName2 = imageName[-1].split(".")
-            image = applyTransformations(image, option[0], option[1])
-            cv2.imwrite(newDir + "/" + imageName2[0] + "_" + option[0] + "_" + str(counter) + ".jpg", image)
+            appliedTransformations = ""
+            options = line.split(';')
+            for op in options:
+                option = op.split(':')
+                parameters = option[1].split(',')
+                image = applyTransformations(image, option[0], parameters)
+                appliedTransformations += option[0] + "_"
+            cv2.imwrite(newDir + "/" + imageName2[0] + "_" + appliedTransformations + str(counter) + ".jpg", image)
             counter += counter
 
 else:
